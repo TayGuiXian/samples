30f71ad3a1fce6395b99807bfa20f5c0ca0453c8
TayGuiXian <b032110019@student.utem.edu.my>
2022-03-10 19:44:52 +0800

    add more attribute

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+	private String msg;
+	
+	void setter(String msg)
+	{
+		this.msg= "testing"; 
+	}
+	
+	String getter(String msg)
+	{
+		return(msg);
+	}
+}
